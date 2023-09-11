3d7ff55d2f78ee8c479f52cb8b0d6ecdb9712ee1
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2023-09-11 21:04:14 +0100

    updates

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -17,11 +17,12 @@ const Sidenav = () => {
   return (
     <div>
       <AiOutlineMenu
+        size={30}
         onClick={handleNav}
-        className="absolute top-4 right-4 z-[99] text-white md:hidden"
+        className="fixed top-4 right-4 z-[99] rounded-sm border-2 border-orange-300 text-orange-300  p-1  md:hidden"
       />
       {nav ? (
-        <div className="fixed w-full h-screen bg-white/30 flex flex-col justify-center items-center z-20">
+        <div className="fixed top-0 left-0 w-full h-screen bg-white/30 flex flex-col justify-center items-center z-20">
           <a
             onClick={handleNav}
             href="#main"
