c4384130a00cbf67988428ba586901f4c6974efc
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2023-11-05 20:10:03 +0000

    services

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,41 @@
+import Services from "./Services";
+
 const About = () => {
   return (
-    <section id="about" className="h-screen bg-slate-300 flex flex-col">
-      <div className="max-w-[1024px] m-auto md:pl-20 p-4 py-16 ">
-        <h2 className="text-4xl font-bold text-center  text-pink-700 mb-4">
-          About
-        </h2>
-        <h3 className="px-4 text-center">
-          Moonflower Labs is a software development company focused on crafting
-          customized solutions for small businesses. Founded by
-          <span className="text-pink-700 text-lg"> Alex Santiago</span>. As a
-          developer, his goal is to create efficient and user-friendly
-          applications. He is passionate about coding and constantly learning to
-          stay up-to-date with the latest technologies. Collaboration and
-          communication are main the priorities, ensuring the understanding and
-          alignment with the vision and goals of our clients. Always committed
-          to delivering projects on time and within budget to achieve client
-          satisfaction. If you are looking for a skilled and dedicated developer
-          to bring your ideas to life,
-          <span> Alex</span> is here to help.
-        </h3>
-      </div>
-    </section>
+    <div className="h-screen bg-slate-300 flex flex-col overflow-y-auto">
+      <section id="about">
+        <div className="max-w-[1024px] m-auto md:pl-20 p-4 py-16 lg:flex flex-row items-center gap-2 lg:gap-12">
+          <h2 className="text-4xl font-bold text-center  text-pink-700 mb-4">
+            About
+          </h2>
+          <div className="flex flex-col gap-4">
+            <p className="px-8 md:px-4 text-justify text-xl">
+              Moonflower Labs is a software development company focused on
+              crafting customized solutions for small businesses. Founded by
+              <span className="text-pink-700 text-lg"> Alex Santiago</span>. As
+              a developer, his goal is to create efficient and user-friendly
+              applications. He is passionate about coding and constantly
+              learning to stay up-to-date with the latest technologies.
+              Collaboration and communication are main the priorities, ensuring
+              the understanding and alignment with the vision and goals of our
+              clients. Always committed to delivering projects on time and
+              within budget to achieve client satisfaction. If you are looking
+              for a skilled and dedicated developer to bring your ideas to life,
+              <span className="text-pink-700 text-lg"> Alex</span> is here to
+              help.
+            </p>
+            <p className="px-8 md:px-4 text-justify text-xl">
+              Moonflower Labs specializes in providing comprehensive services in
+              web design, databases, UI, consulting, and web development. Our
+              expertise in these areas allows us to deliver tailored solutions
+              that meet the specific needs of our clients. Here&apos;s a brief
+              overview of each of these services:
+            </p>
+          </div>
+        </div>
+      </section>
+      <Services />
+    </div>
   );
 };
 
---
+++ src/components/Services.jsx
@@ -0,0 +1,77 @@
+const Services = () => {
+  return (
+    <section className="h-max flex  bg-zinc-600">
+      <div className="max-w-[1096px]  m-auto md:pl-20 p-4 py-16 lg:flex flex-row items-center gap-2 lg:gap-12">
+        <h2 className="text-4xl font-bold text-center  text-pink-700 mb-4">
+          Services
+        </h2>
+        <ul className="text-xl text-justify text-orange-100/95">
+          <li className="mb-10 ml-4">
+            <span className="inline-block px-2 py-1 font-semibold bg-gradient-to-r from-gray-500 to-[#aa7120] rounded-md text-orange-200 mb-4">
+              1. Web Design:
+            </span>
+            <p>
+              Stunning user-friendly websites that not only capture your
+              brand&apos;s essence but also provide an excellent user
+              experience. We focus on creating intuitive interfaces and
+              responsive designs to ensure that your website stands out from the
+              competition.
+            </p>
+          </li>
+          <li className="mb-10 ml-4">
+            <span className="inline-block px-2 py-1 font-semibold bg-gradient-to-r from-gray-500 to-[#aa7120] rounded-md text-orange-200 mb-4">
+              2. Databases
+            </span>
+            <p>
+              We have experience designing and implementing efficient database
+              systems for businesses. Our database solutions are secure,
+              scalable, and optimized to handle large volumes of data. We can
+              assist with database architecture, data modeling, performance
+              tuning, and data integration.
+            </p>
+          </li>
+          <li className="mb-10 ml-4">
+            <span className="inline-block px-2 py-1 font-semibold bg-gradient-to-r from-gray-500 to-[#aa7120] rounded-md text-orange-200 mb-4">
+              3. UI (User Interface) Design
+            </span>
+            <p>
+              We understand the significance of a well-designed user interface
+              in enhancing user engagement and satisfaction. We specialize in
+              creating aesthetically pleasing and intuitive interface designs
+              that enhance usability and drive conversions.
+            </p>
+          </li>
+          <li className="mb-10 ml-4">
+            <span className="inline-block px-2 py-1 font-semibold bg-gradient-to-r from-gray-500 to-[#aa7120] rounded-md text-orange-200 mb-4">
+              4. Consulting
+            </span>
+            <p>
+              Our consulting services empower businesses to make informed
+              technology decisions. We provide expert guidance on software
+              selection, system integration, and technology strategy. Our
+              consultants work closely with clients to understand their unique
+              requirements and provide practical solutions that align with their
+              goals.
+            </p>
+          </li>
+          <li className="mb-10 ml-4">
+            <span className="inline-block px-2 py-1 font-semibold bg-gradient-to-r from-gray-500 to-[#aa7120] rounded-md text-orange-200 mb-4">
+              5. Web Development
+            </span>
+            <p>
+              At{" "}
+              <span className="text-pink-700 font-bold">Moonflower Labs</span>,
+              we can bring your ideas to life by creating robust and functional
+              web applications. We use modern development frameworks and
+              technologies to build scalable and secure solutions. Whether
+              it&apos;s a simple website or a complex web application, we have
+              the expertise to deliver high-quality results.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
